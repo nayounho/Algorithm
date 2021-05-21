ec1483fd481d03e140fb372df7b96ee76bb8ccb1
younho <nnyyhh9247@gmail.com>
2021-05-21 18:31:24 +0900

    배열안 배열의 target 찾기 및 피보나치 수열

--- 정수제곱근판별.js
+++ 정수제곱근판별.js
@@ -3,7 +3,7 @@
 //정수 x의 제곱이 아니라면 ‘no’를 리턴하는 함수를 작성하라.
 //예를 들어, n이 121이라면 이는 정수 11의 제곱이므로 (11+1)의 제곱인 144를 리턴하고, 3이라면 ‘no’을 리턴한다.
 
-function nextSqaure(n){
+const nextSqaure = (n) => {
   const Squared = Math.sqrt(n)
   if (isNaN(n)) {
     return 'no'
