0da711783814c716f0fffd166fa4a6fb58a669bd
younhona <nnyyhh9247@gmail.com>
2021-12-07 18:12:38 +0900

    20211207 algorithm

--- 25.없는숫자 더하기.js
+++ 25.없는숫자 더하기.js
@@ -13,5 +13,5 @@ function solution(numbers) {
       return result;
 }
 
-solution([1,2,3,4,6,7,8,0]); // 14
-solution([5,8,4,0,6,7,9]); // 6
+solution([1,2,3,4,6,7,8,0]); //  14
+solution([5,8,4,0,6,7,9]); //  6
