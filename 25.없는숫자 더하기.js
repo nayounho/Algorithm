760adc088ee817328d0234ef052e67f5bbd57938
younhona <younho.na@atto-research.com>
2021-12-07 18:04:20 +0900

    20211207 algorithm

---
+++ 25.없는숫자 더하기.js
@@ -0,0 +1,17 @@
+// 0부터 9까지의 숫자 중 일부가 들어있는 배열 numbers가 매개변수로 주어집니다. 
+// numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+  const numSort = numbers.sort();  
+  let result = 0;
+  
+  for (let i = 1; i < 10; i++){
+    if(!numSort.includes(i)){
+      result += i;
+    } 
+  }
+      return result;
+}
+
+solution([1,2,3,4,6,7,8,0]); // 14
+solution([5,8,4,0,6,7,9]); // 6
