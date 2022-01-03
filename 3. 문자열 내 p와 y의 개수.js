47d014915ad8e6606b8e16e06dbb2cefeca1bead
younhona <nnyyhh9247@gmail.com>
2022-01-03 11:55:11 +0900

    20220103 algorithm

--- 3. 문자열 내 p와 y의 개수.js
+++ 3. 문자열 내 p와 y의 개수.js
@@ -1,20 +1,58 @@
-//numPY함수는 대문자와 소문자가 섞여있는 문자열 s를 인수로 전달받는다. s에 존재하는 ‘p’의 개수와 ‘y’의 갯수를 비교해 같으면 true, 다르면 false를 리턴하도록 함수를 완성하라. 대소문자를 구별하지 않으며 ‘p’, ‘y’ 모두 하나도 없는 경우는 항상 true를 리턴한다.
-//예를 들어 s가 ‘pPoooyY’면 true를 리턴하고 ‘Pyy’라면 false를 리턴한다.
+// 문제 설명
+// 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 
+// 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 
+// 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+// 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+// 제한사항
+// 문자열 s의 길이 : 50 이하의 자연수
+// 문자열 s는 알파벳으로만 이루어져 있습니다.
+
+// 입출력 예
+// s	             answer
+// "pPoooyY"	     true
+// "Pyy"	         false
+
+// 입출력 예 설명
+// 입출력 예 #1
+// 'p'의 개수 2개, 'y'의 개수 2개로 같으므로 true를 return 합니다.
+// 입출력 예 #2
+// 'p'의 개수 1개, 'y'의 개수 2개로 다르므로 false를 return 합니다.
+
+// ※ 공지 - 2021년 8월 23일 테스트케이스가 추가되었습니다.
 
 
+// 2022.01.03 remind coding
+function solution(s){
+  let strP = 0;
+  let strY = 0;
+  const stringLower = s.toLowerCase().split('');
+  stringLower.forEach(v => {
+    if (v === 'p') {
+      strP += 1
+    } else if(v === 'y') {
+      strY += 1
+    }
+  })
+  return strP === strY ? true : false
+}
 
-function numPY(s) {
-  if ( s === undefined ) return true
+console.log(solution("pPoooyY"))
+console.log(solution("Pyy"))
+
+// 2021.01
+// function numPY(s) {
+//   if ( s === undefined ) return true
   
-    const arr = s.split('')
-    const numP = arr.filter( i => i === 'p' ).length
-    const numY = arr.filter( i => i === 'y' ).length
+//     const arr = s.split('')
+//     const numP = arr.filter( i => i === 'p' ).length
+//     const numY = arr.filter( i => i === 'y' ).length
   
-    return numP === numY;
-  };
+//     return numP === numY;
+//   };
   
-  console.log(numPY('pPoooyY')); // true
-  console.log(numPY('Pyy'));     // false
-  console.log(numPY('ab'));      // true
-  console.log(numPY(''));        // true
-  console.log(numPY());          // true
+//   console.log(numPY('pPoooyY')); // true
+//   console.log(numPY('Pyy'));     // false
+//   console.log(numPY('ab'));      // true
+//   console.log(numPY(''));        // true
+//   console.log(numPY());          // true
